a073ffbbe652ddb9392c3550ff40eeecbf2bc3d5
Markus Kremer <kremer.markus1996@gmail.com>
2021-10-19 22:38:40 +0200

    set window height

--- electron-main.js
+++ electron-main.js
@@ -5,7 +5,8 @@ require('electron-reload')(__dirname);
 
 app.on("ready", () => {
   const mainWindow = new BrowserWindow({
-    width: 1000
+    width: 1000,
+    height: 750
   });
   mainWindow.loadFile(path.join(__dirname, "public/index.html"));
   mainWindow.webContents.openDevTools();
