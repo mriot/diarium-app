4ee1905292729bf8db5077686807dcd154b65bd9
Markus Kremer <kremer.markus1996@gmail.com>
2021-10-20 21:42:18 +0200

    add ipc

--- electron-main.js
+++ electron-main.js
@@ -1,13 +1,27 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain, dialog } = require("electron");
 const path = require("path");
 
 require('electron-reload')(__dirname);
 
-app.on("ready", () => {
-  const mainWindow = new BrowserWindow({
+let mainWindow;
+
+app.whenReady().then(() => {
+  mainWindow = new BrowserWindow({
     width: 1000,
-    height: 750
+    height: 750,
+    frame: true,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      devTools: process.env.ROLLUP_WATCH
+    }
   });
+  
   mainWindow.loadFile(path.join(__dirname, "public/index.html"));
   mainWindow.webContents.openDevTools();
-});
+})
+
+ipcMain.handle("test-me", async (event, args) => {
+  const result = await dialog.showOpenDialog(mainWindow, {title: "asdf"})
+  console.log(result);
+  return result
+})
