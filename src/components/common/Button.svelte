0dd3be062fa03baad6a5a63e1c5e0b95250b5e35
Markus Kremer <kremer.markus1996@gmail.com>
2021-10-22 00:50:22 +0200

    add common components

---
+++ src/components/common/Button.svelte
@@ -0,0 +1,26 @@
+<script>
+  export let value = "";
+</script>
+
+<button on:click>{value}</button>
+
+<style lang="scss">
+  button {
+    position: relative;
+    display: flex;
+    align-items: center;
+    color: #efefef;
+    background-color: #363b47;
+    cursor: pointer;
+    text-decoration: none;
+    padding: 0.75em 3em;
+    margin: 1em auto 0;
+    font-size: 1.25rem;
+    border-radius: 5px;
+    border: none;
+    user-select: none;
+    transition: all 0.2s;
+    white-space: nowrap;
+    text-decoration: none;
+  }
+</style>
