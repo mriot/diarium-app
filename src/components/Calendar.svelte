bcad5b0f8d5eebcf56461e732b17684afef47423
Markus Kremer <kremer.markus1996@gmail.com>
2021-10-26 23:50:18 +0200

    add selected prop

--- src/components/Calendar.svelte
+++ src/components/Calendar.svelte
@@ -2,9 +2,10 @@
   import Datepicker from "praecox-datepicker";
   import "../scss/calendar.scss";
 
+  export let selected;
   export let marked;
   export let disabled;
   export let viewDate;
 </script>
 
-<Datepicker {marked} {disabled} {viewDate} finishBtn={false} />
+<Datepicker {selected} {marked} {disabled} {viewDate} finishBtn={false} />
