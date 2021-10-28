99df3084185c1905e616595418d0b3d3e137d5c3
Markus Kremer <kremer.markus1996@gmail.com>
2021-10-28 19:32:46 +0200

    convert to store-driven modifications

--- src/components/Calendar.svelte
+++ src/components/Calendar.svelte
@@ -2,17 +2,50 @@
   import Datepicker from "praecox-datepicker";
   import { selectedDate } from "../stores/appStore";
   import "../scss/calendar.scss";
+  import { beforeUpdate } from "svelte";
+  const { api } = window; // electron
 
   export let viewDate;
 
-  const { api } = window; // electron
+  let store; // exposed calendar store
 
-  let marked = ["2021-10-20"];
+  // update calendar state when viewDate changes
+  $: {
+    store?.update((prev) => {
+      return { ...prev, viewDate, selected: viewDate };
+    });
+  }
 
   (async () => {
     const records = await api.getAllRecords();
-    marked = records.map((r) => r.date);
+    store.update((prev) => {
+      return { ...prev, focused: records.map((r) => r.date) };
+    });
   })();
 </script>
 
-<Datepicker bind:selected={$selectedDate} {marked} {viewDate} finishBtn={false} />
+<!-- <button
+  on:click={() => {
+    store.update((prev) => {
+      return { ...prev, viewDate: new Date() };
+    });
+  }}>jump to today</button
+>
+
+<button
+  on:click={() => {
+    store.update((prev) => {
+      return { ...prev, focused: [...prev.focused, "2021-11-10", "2021-10-10"] };
+    });
+  }}>add marked</button
+>
+
+<button
+  on:click={() => {
+    store.update((prev) => {
+      return { ...prev, view: "year" };
+    });
+  }}>set view to year</button
+> -->
+
+<Datepicker bind:store bind:selected={$selectedDate} finishBtn={false} />
