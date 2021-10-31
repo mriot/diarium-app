4aa363f37aafb771b7d5323de3ac267cb3094040
Markus Kremer <kremer.markus1996@gmail.com>
2021-10-31 23:31:12 +0100

    add allRecords[]

--- src/stores/appStore.js
+++ src/stores/appStore.js
@@ -3,3 +3,4 @@ import { writable } from "svelte/store";
 export const editMode = writable(false);
 export const selectedDate = writable(new Date());
 export const dayRecord = writable({});
+export const allRecords = writable([]);
