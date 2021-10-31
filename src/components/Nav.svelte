1325d27be2598dcd62edb9f08e82f83d02dcf5d2
Markus Kremer <kremer.markus1996@gmail.com>
2021-10-31 12:58:24 +0100

    nav theming

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,7 +1,6 @@
 <script>
   import Fa from "svelte-fa";
   import { faPen, faPlus, faSave, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
-  import TextInput from "./common/TextInput.svelte";
   import { editMode, dayRecord } from "../stores/appStore";
 </script>
 
@@ -20,8 +19,9 @@
       {/if}
     </li>
     <li>
-      <TextInput class="input" placeholder="Search..." />
+      <input type="text" class="search-input" placeholder="Search..." />
     </li>
+    <li class="separator" />
     <li class="nav-button"><Fa icon={faSignOutAlt} /></li>
   </ul>
 </nav>
@@ -32,43 +32,69 @@
     display: flex;
     justify-content: space-between;
     align-items: center;
+    color: #9e9e9e;
     border-bottom: 1px solid #191919;
+  }
 
-    h1 {
-      margin: 0.3em;
-      font-size: 2rem;
+  h1 {
+    color: #fff;
+    margin: 0.3em;
+    font-size: 2rem;
+  }
+
+  ul {
+    display: flex;
+    align-items: center;
+    gap: 0.5em;
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+
+    li {
+      margin: 0 0.5em;
+    }
+  }
+
+  .separator {
+    width: 1px;
+    align-self: stretch;
+    margin: 0.5em;
+    margin-right: -1em;
+    background-color: #424347;
+  }
+
+  .nav-button {
+    position: relative;
+    cursor: pointer;
+    padding: 0.3em 1.7em;
+    border-radius: 3px;
+    border-bottom: 1px solid transparent;
+    backface-visibility: hidden;
+    user-select: none;
+    transition: all 0.2s;
+
+    &.active {
+      color: #00b7ff;
+      border-color: #00b7ff;
+      border-radius: 3px 3px 0 0;
     }
 
-    ul {
-      display: flex;
-      align-items: center;
-      gap: 0.5em;
-      margin: 0;
-      padding: 0;
-      list-style-type: none;
+    &:hover {
+      background-color: #33363e;
     }
+  }
 
-    .nav-button {
-      position: relative;
-      cursor: pointer;
-      color: #9e9e9e;
-      padding: 7px 20px;
-      margin: 0 5px;
-      border-radius: 3px;
-      border-bottom: 1px solid transparent;
-      backface-visibility: hidden;
-      user-select: none;
-      transition: all 0.2s;
-
-      &.active {
-        color: #00b7ff;
-        border-color: #00b7ff;
-        border-radius: 3px 3px 0 0;
-      }
-
-      &:hover {
-        background-color: #33363e;
-      }
-    }
+  .search-input {
+    appearance: none;
+    outline: none;
+    padding: 0.5em 1em;
+    font-size: inherit;
+    color: inherit;
+    margin-top: 0;
+    border: none;
+    border-radius: 3px;
+    box-shadow: 0 1px 3px rgb(0 0 0 / 12%), 0 1px 2px rgb(0 0 0 / 24%);
+    background-color: #424347;
+    transition: border 0.3s, box-shadow 0.2s, color 0.5s;
   }
 </style>
