9247d80508388be3b259d1a5875358d65abb14fa
Markus Kremer <kremer.markus1996@gmail.com>
2021-11-01 16:42:43 +0100

    add sync methods for config

--- src/electron/config-manager.js
+++ src/electron/config-manager.js
@@ -1,6 +1,7 @@
 const { app, dialog } = require("electron");
 const path = require("path");
 const { readFile, writeFile, stat } = require("fs/promises");
+const { writeFileSync, readFileSync } = require("fs");
 
 // INIT
 module.exports = {
@@ -37,6 +38,32 @@ module.exports = {
       writeFile(this._configPath, JSON.stringify(config));
     } catch (error) {
       console.log("config.write() failed!", error);
+
+  getSync(key) {
+    try {
+      const configString = readFileSync(this._configPath, { encoding: "utf-8" });
+      const config = JSON.parse(configString);
+      return key ? config[key] : config;
+    } catch (error) {
+      console.log("config.getSync() failed!", error);
+      dialog.showMessageBox(null, {
+        message: "Ooops, couldn't read the config file!",
+        type: "error",
+      });
     }
-  }
+  },
+
+  writeSync(key, value) {
+    try {
+      const config = this.getSync();
+      config[key] = value;
+      writeFileSync(this._configPath, JSON.stringify(config));
+    } catch (error) {
+      console.log("config.writeSync() failed!", error);
+      dialog.showMessageBox(null, {
+        message: "Ooops, couldn't write to the config file!",
+        type: "error",
+      });
+    }
+  },
 };
